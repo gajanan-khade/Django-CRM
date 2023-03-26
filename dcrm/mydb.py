45d285bc69406eb9c2b092c06b46c35152c86f92
gajanan-khade <khadegajanan321@gmail.com>
2023-03-26 19:33:58 +0530

    Intitial Commit

---
+++ dcrm/mydb.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = "localhost",
+	user = "root",
+	password = "8600420079")
+
+
+
+# prepare a cursor object
+
+cursorObject = dataBase.cursor()
+
+
+# create a database
+
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done!!!")
